633162973c6c5f8819debca2284205249df54437
shivTanikella <tanikellashiv@gmail.com>
2025-08-06 01:14:31 -0400

    Debugging styled-components/macro import failure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Jumbotron from './components/jumbotron/index.js'
+
+function App() {
+  return (
+    <div className="app">
+      <Jumbotron />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Inner } from './styles/jumbotron.js';
+
+function Jumbotron(){
+    return (
+        <Inner>
+            <p>hello</p>
+        </Inner>
+    )
+}
+
+export default Jumbotron
