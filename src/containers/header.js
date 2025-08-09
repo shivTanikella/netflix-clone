723fea4ec2ac69df752d1e26a7548ab86b5b4a89
shivTanikella <tanikellashiv@gmail.com>
2025-08-09 01:02:08 -0400

    Working on home page

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {Header} from '../components/header'
+import {Header} from '../components/index.js'
+import * as ROUTES from '../constants/routes.js';
+import logo from '../logo.svg'
 
-export default function HeaderContainer(){
+export default function HeaderContainer({children}){
     return (
         <Header>
             <Header.Frame>
-                <Header.Logo />
-                <Header.Button/>
+                <Header.Logo to={ROUTES.HOME} src={logo} alt="Netflix"/>
+                <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign in</Header.ButtonLink>
             </Header.Frame>
+            {children}
         </Header>
     )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {JumbotronContainer} from '../containers/jumbotron.js'
 import {FooterContainer} from '../containers/footer.js'
 import { FaqsContainer } from '../containers/faqs.js';
+import HeaderContainer from '../containers/header.js';
 
 export default function Home(){
     return(
         <>
-        <JumbotronContainer/>
-        <FaqsContainer/>
-        <FooterContainer/>
+        <HeaderContainer>
+            <JumbotronContainer/>
+            <FaqsContainer/>
+            <FooterContainer/>
+        </HeaderContainer>
         </>
     )
 }
