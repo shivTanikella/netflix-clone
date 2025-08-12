5ee273464900d1a2bdf788c8f015c0218813bb58
shivTanikella <tanikellashiv@gmail.com>
2025-08-12 18:16:53 -0400

    Created  main show page in browser

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,29 +2,57 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { auth } from '../lib/firebase.prod';
-import { onAuthStateChanged } from 'firebase/auth';
-import { Loading } from '../components'
+import {Header,Loading } from '../components'
+import * as ROUTES from '../constants/routes';
+import logo from '../logo.svg';
 
 export function BrowseContainer({ slides }) {
+  const [searchTerm, setSearchTerm]=useState('');
   const [profile, setProfile] = useState({});
   const [loading, setLoading] = useState(true);
   const user = auth.currentUser || {};
 
     useEffect(()=>{
-        console.log('profile', profile)
         setTimeout(()=>{
             setLoading(false);
         },3000);
     }, [profile.displayName]);
 
-  // useEffect(() => {
-  //   const unsub = onAuthStateChanged(auth, (u) => setUser(u || null));
-  //   return () => unsub();
-  // }, []);
-
-  // if (!user) return null;
-
   return profile.displayName ? (
-    loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody/>
-  ) : <SelectProfileContainer user={user} setProfile={setProfile} />;
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody/>}
+      <Header src="joker1" dontShowOnSmallViewPort>
+        <Header.Frame>
+            <Header.Group>
+              <Header.Logo to={ROUTES.HOME} src={logo} alr="Netflix"/>
+              <Header.TextLink>Series</Header.TextLink>
+              <Header.TextLink>Films</Header.TextLink>
+            </Header.Group>
+            <Header.Group>
+              <Header.Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+              <Header.Profile>
+                <Header.Picture src={user.photoURL}/>
+                <Header.Dropdown>
+                  <Header.Group>
+                    <Header.Picture src={user.photoURL}/>
+                    <Header.TextLink>{user.displayName}</Header.TextLink>
+                  </Header.Group>
+                  <Header.Group>
+                    <Header.TextLink onClick={() => auth.signOut()}>Sign Out</Header.TextLink>
+                  </Header.Group>
+                </Header.Dropdown>
+              </Header.Profile>
+            </Header.Group>
+        </Header.Frame>
+
+        <Header.Feature>
+          <Header.FeatureCallOut>Watch Joker Now</Header.FeatureCallOut>
+          <Header.Text>
+            Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a futile attempt to feel like he's part of the world around him.
+          </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
+        </Header.Feature>
+      </Header>
+    </>
+  ) : (<SelectProfileContainer user={user} setProfile={setProfile} />);
 }
