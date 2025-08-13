17ec4c3c0cf75216a74aba41a830549ba276a49f
shivTanikella <tanikellashiv@gmail.com>
2025-08-13 17:43:19 -0400

    error: big picture of every film doesnt pop up when you click on it

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -132,7 +132,7 @@ export const PlayButton = styled.button`
     font-size: 20px;
     margin-top: 30px;
     cursor: pointer;
-    transition: background-color 0.4s ease;
+    transition: background-color 0.5s ease;
 
     &:hover{
         background-color: #ff1e1e;
